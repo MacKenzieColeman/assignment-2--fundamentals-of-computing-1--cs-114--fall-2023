8080c0bec1fcb4c3ac8bf7377d043ec4cc7fcae1
MacKenzieColeman <macoleman@hartford.edu>
2023-10-10 22:27:57 -0400

    odd diamond

--- Diamond.java
+++ Diamond.java
@@ -7,10 +7,34 @@ public class Diamond {
     System.out.print("Enter a Number Here: ");
     int numberString = scan.nextInt();
 
-    if(numberString % 2 == 0)
-      System.out.print("even\n");
+    if(numberString % 2 == 0) {
+      System.out.print("even\n"); }
 
-    else
-      System.out.print("odd\n");
+    else {
+      //top part of diamond
+      for(int i = 1; i <= numberString / 2 + 1; i++) {
+        for(int spaces = 1; spaces <= numberString / 2 + 1 - i; spaces++) {
+          System.out.print(" ");
+        }
+
+        for(int stars = 1; stars <= 2 * i - 1; stars++) {
+          System.out.print("*");
+        }
+
+        System.out.println();
+      }
+      //bottom part of diamond
+      for(int i = numberString / 2; i >= 1; i--) {
+        for(int spaces = 1; spaces <= numberString / 2 + 1 - i; spaces++) {
+          System.out.print(" ");
+        }
+
+        for(int stars = 1; stars <= 2 * i - 1; stars++) {
+          System.out.print("*");
+        }
+
+        System.out.println();
+      }
+    }
   }
 }
