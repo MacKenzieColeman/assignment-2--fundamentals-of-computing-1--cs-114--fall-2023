a2f5b07962f0f515d8fe2c5f87de3ca6a1ec97ae
MacKenzieColeman <macoleman@hartford.edu>
2023-10-10 21:02:03 -0400

    enter number, detect if number is even or odd

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Enter a Number Here: ");
+    int numberString = scan.nextInt();
+
+    if(numberString % 2 == 0)
+      System.out.print("even\n");
+
+    else
+      System.out.print("odd\n");
   }
 }
